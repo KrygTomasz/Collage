53682af60b9d33e583af541de5ae15ff38bfab86
Tomasz Kryg <tomasz.kryg@siroccomobile.com>
2020-01-04 13:46:28 +0100

    Initial commit

---
+++ Collage/Collage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Collage
+//
+//  Created by Tomasz Kryg on 04/01/2020.
+//  Copyright © 2020 Tomasz Kryg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Collage")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
